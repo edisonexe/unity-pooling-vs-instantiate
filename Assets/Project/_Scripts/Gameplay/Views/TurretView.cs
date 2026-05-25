c89b385965bb29a4fb279d391e9e8e7dcca68c1a
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-05-25 19:48:58 +0700

    feat: add TurretView and setup Turret prefab

---
+++ Assets/Project/_Scripts/Gameplay/Views/TurretView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace PoolingBenchmark.Gameplay.Views
+{
+    [AddComponentMenu("PoolingBenchmark/Views/Turret View")]
+    public sealed class TurretView : MonoBehaviour, IInitializable
+    {
+        [Header("References")]
+        [SerializeField] private Transform _yawRoot;
+        [SerializeField] private Transform _shootingPoint;
+
+        public Transform YawRoot => _yawRoot;
+        public Transform ShootingPoint => _shootingPoint;
+
+        public void Initialize()
+        {
+            if (!_yawRoot) Debug.LogError("[TurretView] YawRoot Transform is unassigned!", this);
+            if (!_shootingPoint) Debug.LogError("[TurretView] ShootingPoint Transform is unassigned!", this);
+        }
+    }
+}
