c7195217556f1be8be2b8d6f55fb779d39fddab1
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-05-25 19:53:42 +0700

    feat: add ProjectileManager and TargetManager update loops

---
+++ Assets/Project/_Scripts/Gameplay/Systems/TargetManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using PoolingBenchmark.Domain;
+using PoolingBenchmark.Gameplay.Entities;
+using UnityEngine;
+using Zenject;
+
+namespace PoolingBenchmark.Gameplay.Systems
+{
+    public sealed class TargetManager : ITickable
+    {
+        private readonly EntityRegistry _registry;
+
+        public TargetManager(EntityRegistry registry)
+        {
+            _registry = registry ?? throw new ArgumentNullException(nameof(registry));
+        }
+
+        public void Tick()
+        {
+            float deltaTime = Time.deltaTime;
+            IReadOnlyList<Target> targets = _registry.Targets;
+            int count = targets.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                Target t = targets[i];
+
+                t.transform.Translate(t.MoveDirection * (t.Speed * deltaTime), Space.World);
+            }
+        }
+    }
+}
