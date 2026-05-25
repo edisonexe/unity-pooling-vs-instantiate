1206b48c62e791e1d8bcfa76cc41f3d69ff28bfd
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-05-25 19:57:06 +0700

    feat: add SimulationStartup entry point

---
+++ Assets/Project/_Scripts/Bootstrap/SimulationStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using PoolingBenchmark.Gameplay.Services;
+using Zenject;
+
+namespace PoolingBenchmark.Bootstrap
+{
+    public sealed class SimulationStartup : IInitializable
+    {
+        private readonly TargetSpawner _targetSpawner;
+
+        public SimulationStartup(TargetSpawner targetSpawner)
+        {
+            _targetSpawner = targetSpawner ?? throw new ArgumentNullException(nameof(targetSpawner));
+        }
+
+        public void Initialize()
+        {
+            _targetSpawner.StartSpawning();
+        }
+    }
+}
