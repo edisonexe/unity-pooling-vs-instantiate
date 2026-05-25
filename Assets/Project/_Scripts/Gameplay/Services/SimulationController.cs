695f12f2a0895f99e26722444c3ad6f7590d5f47
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-05-25 19:55:26 +0700

    feat: add SimulationController and StatsCollector

---
+++ Assets/Project/_Scripts/Gameplay/Services/SimulationController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using PoolingBenchmark.Interfaces;
+using PoolingBenchmark.Enums;
+using PoolingBenchmark.Gameplay.Systems;
+
+namespace PoolingBenchmark.Gameplay.Services
+{
+    public sealed class SimulationController : ISimulationController
+    {
+        private readonly IEntityFactory _factory;
+        private readonly StatsCollector _collector;
+        private readonly TargetSpawner _spawner;
+        private readonly PoolService _pools;
+
+        private ExecutionMode _currentMode = ExecutionMode.Naive;
+
+        public SimulationController(
+            IEntityFactory factory, 
+            StatsCollector collector, 
+            TargetSpawner spawner, 
+            PoolService pools)
+        {
+            _factory = factory ?? throw new ArgumentNullException(nameof(factory));
+            _collector = collector ?? throw new ArgumentNullException(nameof(collector));
+            _spawner = spawner ?? throw new ArgumentNullException(nameof(spawner));
+            _pools = pools ?? throw new ArgumentNullException(nameof(pools));
+            
+            ApplyMode(_currentMode);
+        }
+
+        public void ToggleMode()
+        {
+            _spawner.StopSpawning();
+            _factory.Cleanup();
+
+            _currentMode = _currentMode == ExecutionMode.Naive ? ExecutionMode.Pool : ExecutionMode.Naive;
+            
+            ApplyMode(_currentMode);
+            _spawner.StartSpawning();
+        }
+
+        private void ApplyMode(ExecutionMode mode)
+        {
+            if (mode == ExecutionMode.Pool)
+            {
+                _pools.Prewarm();
+            }
+
+            _factory.SetMode(mode);
+            _factory.ResetCounter();
+            _collector.SetMode(mode);
+        }
+    }
+}
---
+++ Assets/Project/_Scripts/Gameplay/Services/StatsCollector.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using PoolingBenchmark.Domain;
+using PoolingBenchmark.Gameplay.Systems;
+using PoolingBenchmark.Interfaces;
+using PoolingBenchmark.Enums;
+using Zenject;
+
+namespace PoolingBenchmark.Gameplay.Services
+{
+    public sealed class StatsCollector : IStatsProvider, ITickable, IDisposable
+    {
+        private const float UPDATE_INTERVAL = 0.1f;
+
+        private readonly PoolService _pools;
+        private readonly EntityRegistry _registry;
+        private readonly IEntityFactory _entityFactory;
+        
+        private ExecutionMode _currentMode;
+        private float _timer;
+        private bool _isDirty;
+
+        public event Action<SimulationStats> OnStatsChanged;
+
+        public StatsCollector(PoolService pools, EntityRegistry registry, IEntityFactory entityFactory)
+        {
+            _pools = pools ?? throw new ArgumentNullException(nameof(pools));
+            _registry = registry ?? throw new ArgumentNullException(nameof(registry));
+            _entityFactory = entityFactory ?? throw new ArgumentNullException(nameof(entityFactory));
+
+            _registry.OnChanged += SetDirty;
+        }
+
+        public void SetMode(ExecutionMode mode)
+        {
+            _currentMode = mode;
+            SetDirty();
+        }
+
+        public void Tick()
+        {
+            float deltaTime = UnityEngine.Time.deltaTime;
+            _timer += deltaTime;
+
+            if (_isDirty && _timer >= UPDATE_INTERVAL)
+            {
+                UpdateStats();
+                _timer = 0f;
+                _isDirty = false;
+            }
+        }
+
+        public void UpdateStats()
+        {
+            bool isPool = _currentMode == ExecutionMode.Pool;
+            
+            int totalProjs = isPool ? _pools.ProjectilePool.TotalCreated : _entityFactory.ProjNaiveCounter;
+            int totalTargets = isPool ? _pools.TargetPool.TotalCreated : _entityFactory.TargetNaiveCounter;
+
+            SimulationStats stats = new SimulationStats(
+                _currentMode,
+                _registry.Projectiles.Count,
+                _registry.Targets.Count,
+                totalProjs,                               
+                totalTargets,                             
+                _pools.ProjectilePool.TotalCreated,       
+                _pools.TargetPool.TotalCreated,         
+                _pools.ProjectilePool.AvailableCount,
+                _pools.TargetPool.AvailableCount,
+                _pools.ProjectilePool.ReusedCount,
+                _pools.TargetPool.ReusedCount
+            );
+
+            OnStatsChanged?.Invoke(stats);
+        }
+
+        private void SetDirty() => _isDirty = true;
+
+        public void Dispose()
+        {
+            if (_registry != null) _registry.OnChanged -= SetDirty;
+        }
+    }
+}
