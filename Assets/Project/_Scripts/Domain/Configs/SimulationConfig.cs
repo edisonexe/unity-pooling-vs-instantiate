b5c5d0dd0154acdfa0a6b9a90c9bb396f7726e3f
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-05-25 19:24:18 +0700

    feat: create SimulationConfig scriptable object

---
+++ Assets/Project/_Scripts/Domain/Configs/SimulationConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace PoolingBenchmark.Domain.Configs
+{
+    [CreateAssetMenu(fileName = "SimulationConfig", menuName = "PoolingBenchmark/Simulation Config")]
+    public sealed class SimulationConfig : ScriptableObject
+    {
+        [Header("Pool Options")]
+        [SerializeField] private int _prewarmCount = 500;
+
+        [Header("Spawner Options")]
+        [SerializeField] private float _spawnInterval = 0.5f;
+
+        [Header("Turret Options")]
+        [SerializeField] private float _fireRate = 0.01f;
+        [SerializeField] private float _fovAngle = 120f;
+
+        [Header("Spawn Points Options")]
+        [SerializeField] private int _spawnPointsCacheSize = 100;
+        
+        public int PrewarmCount => _prewarmCount;
+        public float SpawnInterval => _spawnInterval;
+        public float FireRate => _fireRate;
+        public float FovAngle => _fovAngle;
+        public int SpawnPointsCacheSize => _spawnPointsCacheSize;
+    }
+}
